bbe9dd6727c18486b747cc7ddf296a317a8c3f43
Marchal Florent <florent.marchal@etu.umontpellier.fr>
2025-09-19 10:10:44 +0200

    SFBI now should be able to scrap all jobs and not obly the 25 first ones

--- job_scrapper/scrap_job_from/sfbi.py
+++ job_scrapper/scrap_job_from/sfbi.py
@@ -1,8 +1,22 @@
 import re
+import time
 
+import bs4
 from bs4 import BeautifulSoup
 
 import job_scrapper.scrapper_skeleton.scrapper_skeleton as srk
+from typing import Generator
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import (
+    ElementClickInterceptedException,
+    StaleElementReferenceException,
+    TimeoutException,
+)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class SFBIScrapper(srk.JobScrapperSkeleton):
@@ -12,6 +26,7 @@ class SFBIScrapper(srk.JobScrapperSkeleton):
 
     website_url = "https://www.sfbi.fr/emplois/offres-en-cours"
     job_across_multiple_pages = True
+    job_across_multiple_pages_mandatory_action = True
     HEADERS = [
         "title",  # "Titre",
         "Niveau",
@@ -26,10 +41,50 @@ class SFBIScrapper(srk.JobScrapperSkeleton):
     ]
 
     @classmethod
-    def extract_block_of_interest(cls, soup) -> BeautifulSoup:
+    def extract_block_of_interest(cls, soup) -> list[BeautifulSoup]:
         return soup.find(
             name="table",
         )
+
+    @classmethod
+    def _job_across_multiple_pages_command_action(cls) -> list[bs4.BeautifulSoup]:
+        browser = cls.open_url_inside_browser(cls.website_url)
+        is_disabled = False
+        all_pages = []
+        i = 1
+        while not is_disabled:
+            cls._rough_page_parsing_actions(browser)
+            if i == 1:
+                cls.logger.debug("Loading page %s", i)
+                html = browser.page_source
+                soup = BeautifulSoup(html, "html.parser")
+                all_pages.append(soup)
+            i += 1
+
+            next_page_btn = WebDriverWait(browser, 5).until(
+                    EC.element_to_be_clickable(
+                        (
+                            By.CSS_SELECTOR,
+                            "button.page-link.next",
+                        )
+                    )
+                )
+            is_disabled = next_page_btn.get_attribute("aria-disabled")
+
+            if not is_disabled:
+                next_page_btn.click()
+                cls.logger.debug("Loading page %s", i)
+                html = browser.page_source
+                soup = BeautifulSoup(html, "html.parser")
+                all_pages.append(soup)
+                time.sleep(cls.sleep_during_page_loading)
+
+
+            else:
+                cls.logger.debug("No page %s, closing browser on %s", i + 1, cls.website_url)
+                browser.close()
+
+        return all_pages
 
     @classmethod
     def complete_job_page_parsing(
