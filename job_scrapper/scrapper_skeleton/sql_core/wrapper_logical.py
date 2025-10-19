8cd9063d786759a8330f4b71793a04c122fb42b8
F-Marchal <marchal.florent.pro@gmail.com>
2025-10-19 14:49:00 +0200

    feat(sqlcore/wrapper_*): rename CommandWrapper into ClauseElementWrapper. ClauseElementWrapper split between ComparisonWrapper and LogicalWrapper
    Enanced typing and complete test phases added. #17

---
+++ job_scrapper/scrapper_skeleton/sql_core/wrapper_logical.py
@@ -0,0 +1,67 @@
+from sqlalchemy import and_, or_, not_
+from typing import Callable
+from sqlalchemy.sql.elements import ClauseElement
+from .wrapper_clause_element import ClauseElementWrapper
+
+class LogicalWrapper(ClauseElementWrapper):
+    """Wrapper for logical operators (and_, or_) taking exactly two conditions."""
+    def __init__(
+            self,
+            op: Callable[[ClauseElement | bool, ClauseElement | bool], ClauseElement],
+            help_: str,
+            symbols: list[str],
+    ):
+        """
+        :param op: a callable object that can be used in a query.filter()
+        :param help_:  A string that give information about the operation done by self.op
+        :param symbols: A symbol that represent the operation done by self.op
+        """
+        super().__init__(op, help_, symbols)
+
+    @property
+    def op(self) -> Callable[[ClauseElement | bool, ClauseElement | bool], ClauseElement]:
+        return self._op
+
+    @op.setter
+    def op(self, operator:  Callable[[ClauseElement | bool, ClauseElement | bool], ClauseElement]):
+        self._op = operator
+
+
+AND_WRAPPER = LogicalWrapper(
+    op=and_,
+    help_="Logical AND (last and current conditions must be true)",
+    symbols=["And", "&"]
+)
+
+OR_WRAPPER = LogicalWrapper(
+    op=or_,
+    help_="Logical OR (last or current condition must be true)",
+    symbols=["Or", "|"]
+)
+
+XOR_WRAPPER = LogicalWrapper(
+    op=lambda first, second: and_(or_(first, second), not_(and_(first, second))),
+    help_="Logical XOR (Only one of the two should be True)",
+    symbols=["Xor", "^"]
+)
+
+AND_NOT_WRAPPER = LogicalWrapper(
+    op=lambda first, second: and_(first, not_(second)),
+    help_="Logical AND NOT (The two conditions should be False)",
+    symbols=["And not", "&~"]
+)
+
+OR_NOT_WRAPPER = LogicalWrapper(
+    op=lambda first, second: or_(first, not_(second)),
+    help_="Logical OR NOT (None of the two conditions can be True)",
+    symbols=["Or not", "|~"]
+)
+LOGICAL_WRAPPERS = [
+    AND_WRAPPER,
+    OR_WRAPPER,
+    XOR_WRAPPER,
+    AND_NOT_WRAPPER,
+    OR_NOT_WRAPPER
+]
+STRING_TO_LOGICAL_WRAPPERS: dict[str, LogicalWrapper] = {s: cw for cw in LOGICAL_WRAPPERS for s in cw.symbols}
+
