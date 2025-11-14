4f1b693d50198f56955ee102ee12d37ccee6709e
F-Marchal <marchal.florent.pro@gmail.com>
2025-11-14 17:50:11 +0100

    ci(test_geolocalisation_online): Mock geopy request #38

--- tests/tools/geolocalisation/test_geolocalisation_online.py
+++ tests/tools/geolocalisation/test_geolocalisation_online.py
@@ -2,26 +2,48 @@ from tools.geolocalisation import Places
 from .base_test_geolocalisation import BaseGeoTest
 import pytest
 from tools.geolocalisation import Geolocalisation
+from geopy.location import Location
+from unittest.mock import patch
 
 @pytest.mark.online_simulation
 class TestGeolocalisationOnline(BaseGeoTest):
-    """Test Geolocalisation's functionalities that require an internet connexion."""
-    def test_request__online(self):
-        """Test to request coordinate of an existing and a non-existing place."""
+    """Test Geolocalisation's functionalities that require an internet connexion.
+    Most function here require to mock (path) geocode request"""
+
+    @patch("geopy.geocoders.Nominatim.geocode")
+    def test_request__not_exist(self, mock_geocode):
         geo = self.make_geo()
-        lat, long = geo.request("Paris, France")
-        failed = geo.request("A place name that, obviously, does not exist")
+        mock_geocode.return_value = None
 
-        assert 48.2 < lat < 49.3
-        assert 2 < long <  2.8
-        assert failed == (None, None)
+        result = geo.request("Paris, France")
+        assert result == (None, None)
 
-    def test_geolocate__online(self):
+    @patch("geopy.geocoders.Nominatim.geocode")
+    def test_request__exist(self, mock_geocode):
+        geo = self.make_geo()
+        mock_geocode.return_value = Location(
+            "Paris, France",
+            (48.8566, 2.3522),
+        {}
+        )
+
+        lat, lon = geo.request("Paris, France")
+
+        assert lat == 48.8566
+        assert lon == 2.3522
+
+    @patch("geopy.geocoders.Nominatim.geocode")
+    def test_geolocate__online(self, mock_geocode):
         """Test geolocate with default configuration"""
         geo = self.make_geo()
 
         # Default condition
         with Places.get_session(self.get_db_path(), logger=self.icl.logger) as session:
+            mock_geocode.return_value = Location(
+                "Paris, France",
+                (48.8566, 2.3522),
+                {}
+            )
             lat, long = geo.geolocate(session, "Paris, France")
 
         assert lat is not None
