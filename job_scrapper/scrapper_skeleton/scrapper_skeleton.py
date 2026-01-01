c8067d6ca7b62fe0439f56fee3262111243a1ad4
F-Marchal <marchal.florent.pro@gmail.com>
2026-01-02 00:36:50 +0100

    feat(JobScrapperSkeleton): If an inspection has been done less than `re_inspect_after` days in the past, this inspection is ignored
    #52

--- job_scrapper/scrapper_skeleton/scrapper_skeleton.py
+++ job_scrapper/scrapper_skeleton/scrapper_skeleton.py
@@ -7,6 +7,8 @@ from typing import Self, Iterator
 import re
 from .request_core import ScrapperRequestCore, KeywordManager, ExportBrowserPage, BeautifulSoup
 from sql.tables.keywords.keyword_version import KeywordVersion, Session
+import time
+from datetime import datetime, timedelta
 
 class JobScrapperSkeleton(ScrapperRequestCore):
     @classmethod
@@ -22,6 +24,8 @@ class JobScrapperSkeleton(ScrapperRequestCore):
             search_offer_html: bool = True,
             retry_offer_fetch: int = 2,
             failed_sleep: int = 5,
+
+            re_inspect_after: int = 30,
 
             batch_export: int = 20,
             database_name: str | None = None,
@@ -62,68 +66,174 @@ class JobScrapperSkeleton(ScrapperRequestCore):
         tsv_file = cls.get_unique_path(os.path.join(tsv_folder, f"{cls.strftime(cls.now())}.tsv"))
         offer_batch = []
         i = 0
-        for i, offers in enumerate(cls.extract_offers_from_website()):
-            if offers is None:
+        for i, offer in enumerate(cls.extract_offers_from_website()):
+            if offer is None:
                 # An offer should be ignored see extract_offers_from_website logging.
+                offer_batch.append(None)
                 continue
 
-            cls.logger.info("Processing offer %s : %s", i + 1, offers)
-            cls.logger.debug("Proceeding to offer inspection of %s (%s)", offers, i + 1)
-            offers.offer_inspection(
-                keywords_to_search=keywords_to_search,
-                page_exporter=page_exporter,
-                search_offer_html=search_offer_html,
-                search_offer_text=search_offer_text,
+            cls.logger.debug("Processing offer %s : %s", i + 1, offer)
+
+            if (
+                    keywords_to_search is None
+                    and page_exporter is None
+                    and search_offer_text is False
+                    and search_offer_html is False
+            ):
+                # We do not need to run offer inspection,
+                # There is nothing that should be inspected
+                pass
+
+            else:
+                cls.run_offer_inspection(
+                    offer=offer,
+                    offer_id=i,
+
+                    global_page_exporter=page_exporter,
+                    global_search_offer_html=search_offer_html,
+                    global_keywords_to_search=keywords_to_search,
+                    global_search_offer_text=search_offer_text,
+
+                    re_inspect_after = re_inspect_after,
+                    retry_offer_fetch = retry_offer_fetch,
+                    failed_sleep = failed_sleep,
+
+                )
+
+            offer_batch.append(offer)
+
+            if len(offer_batch) >= batch_export:
+                cls.run_exportation(
+                    sql_export=sql_export,
+                    tsv_export=tsv_export,
+                    tsv_file=tsv_file,
+                    offer_batch=offer_batch,
+                    i=i,
+                    keyword_ver=keyword_ver,
+                    database_name=database_name,
+                    workdir=workdir
+                )
+                offer_batch.clear()
+
+        # Ensure that all items in offer_batch
+        # Have been exported
+        cls.run_exportation(
+            sql_export=sql_export,
+            tsv_export=tsv_export,
+            tsv_file=tsv_file,
+            offer_batch=offer_batch,
+            i=i,
+            keyword_ver=keyword_ver,
+            database_name=database_name,
+            workdir=workdir
+        )
+
+    @classmethod
+    def run_exportation(
+            cls,
+            sql_export: bool,
+            tsv_export: bool,
+            tsv_file: str,
+            offer_batch: list[Self],
+            i: int,
+            keyword_ver: dict[str, KeywordVersion],
+
+            database_name: str | None = None,
+            workdir: str | None = None,
+
+
+    ):
+        if not any(x is not None for x in offer_batch):
+            return
+
+        if sql_export:
+            cls._run_export_sql(
+                offer_batch=offer_batch,
+                database_name=database_name,
+                workdir=workdir,
+                i=i,
+                keywords_ver=keyword_ver,
+            )
+        if tsv_export:
+            cls._run_export_tsv(
+                offer_batch=offer_batch,
+                i=i,
+                tsv_file=tsv_file,
+            )
+        offer_batch.clear()
+
+    @classmethod
+    def run_offer_inspection(
+            cls,
+            offer: Self,
+            offer_id: int,
+
+            global_keywords_to_search: KeywordManager | None = None,
+            global_page_exporter: ExportBrowserPage | None = None,
+            global_search_offer_text: bool = True,
+            global_search_offer_html: bool = True,
+
+            retry_offer_fetch: int = 2,
+            failed_sleep: int = 5,
+            re_inspect_after: int = 30,
+    ) -> bool:
+            # Apply timestamp constraint ()
+            (
+                offer_keywords_to_search,
+                offer_page_exporter,
+                offer_search_offer_text,
+                offer_search_offer_html
+            ) = cls._apply_time_stamp_constraint(
+                offer=offer,
+                offer_id=offer_id,
+
+                keywords_to_search=global_keywords_to_search,
+                page_exporter=global_page_exporter,
+                search_offer_text=global_search_offer_text,
+                search_offer_html=global_search_offer_html,
+
+                re_inspect_after=re_inspect_after,
+            )
+
+            # Avoid necessary requests
+            if (
+                    offer_keywords_to_search is None
+                    and offer_page_exporter is None
+                    and offer_search_offer_text is False
+                    and offer_search_offer_html is False
+            ):
+                cls.logger.info(
+                    "Ignoring processing of '%s' ('%s') since  all the inspections that needed to be "
+                    "performed were already completed less than %s days ago.",
+                    offer, offer_id + 1, re_inspect_after
+                )
+
+                return False
+
+
+            cls.logger.info("Proceeding to offer inspection of %s (%s)", offer, offer_id + 1)
+            offer.offer_inspection(
+                keywords_to_search=offer_keywords_to_search,
+                page_exporter=offer_page_exporter,
+                search_offer_html=offer_search_offer_text,
+                search_offer_text=offer_search_offer_html,
                 retry=retry_offer_fetch,
                 failed_sleep=failed_sleep,
             )
-            offer_batch.append(offers)
+            return True
 
-            if len(offer_batch) >= batch_export:
-                if sql_export:
-                    cls._run_export_sql(
-                        offer_batch=offer_batch,
-                        database_name=database_name,
-                        workdir=workdir,
-                        i=i,
-                        keywords_ver=keyword_ver,
-                    )
-                if tsv_export:
-                    cls._run_export_tsv(
-                        offer_batch=offer_batch,
-                        i=i,
-                        tsv_file=tsv_file,
-                    )
-                offer_batch.clear()
-
-        if offer_batch:
-            if sql_export:
-                cls._run_export_sql(
-                    offer_batch=offer_batch,
-                    database_name=database_name,
-                    workdir=workdir,
-                    i=i,
-                    keywords_ver=keyword_ver,
-                )
-            if tsv_export:
-                cls._run_export_tsv(
-                    offer_batch=offer_batch,
-                    i=i,
-                    tsv_file=tsv_file,
-                )
-            offer_batch.clear()
 
 
     @classmethod
     def _run_export_tsv(
             cls,
-            offer_batch: list[Self],
+            offer_batch: list[Self | None],
             i: int,
             tsv_file: str,
     ):
 
-        offer_to_text = '\n'.join([f"{(i + 1) - len(offer_batch) + (oi + 1)} {o})" for oi, o in enumerate(offer_batch)])
-        cls.logger.debug("Proceeding to sql exportation of %s offers :\n"
+        offer_to_text = '\n'.join([f"{(i + 1) - len(offer_batch) + (oi + 1)} {o})" for oi, o in enumerate(offer_batch) if o is not None])
+        cls.logger.info("Proceeding to tsv exportation of %s offers :\n"
                          "%s",
                          len(offer_batch),
                          f"{offer_to_text}"
@@ -133,7 +243,7 @@ class JobScrapperSkeleton(ScrapperRequestCore):
     @classmethod
     def _run_export_sql(
             cls,
-            offer_batch: list[Self],
+            offer_batch: list[Self | None],
             database_name: str,
             workdir: str,
             i: int,
@@ -141,8 +251,8 @@ class JobScrapperSkeleton(ScrapperRequestCore):
 
 
     ):
-        offer_to_text = '\n'.join([f"{(i + 1) - len(offer_batch) + (oi + 1)} {o})" for oi, o in enumerate(offer_batch)])
-        cls.logger.debug("Proceeding to sql exportation of %s offers :\n"
+        offer_to_text = '\n'.join([f"{(i + 1) - len(offer_batch) + (oi + 1)} {o})" for oi, o in enumerate(offer_batch) if o is not None])
+        cls.logger.info("Proceeding to sql exportation of %s offers :\n"
                          "%s",
                          len(offer_batch),
                          f"{offer_to_text}"
@@ -153,6 +263,54 @@ class JobScrapperSkeleton(ScrapperRequestCore):
             workdir=workdir,
             keywords_ver=keywords_ver,
         )
+
+    @classmethod
+    def _apply_time_stamp_constraint(
+            cls,
+            offer: Self,
+            offer_id: int,
+
+            keywords_to_search: KeywordManager | None = None,
+            page_exporter: ExportBrowserPage | None = None,
+            search_offer_text: bool = True,
+            search_offer_html: bool = True,
+            re_inspect_after: int = -1,
+    ) -> tuple[
+        KeywordManager | None,
+        ExportBrowserPage | None,
+        bool,
+        bool
+    ]:
+        threshold = datetime.now() - timedelta(days=re_inspect_after)
+        msg = []
+
+        if keywords_to_search is not None and offer.timestamp_is_too_old(offer.URLInspectionTimeStamp.KEYWORD, threshold):
+            keywords_to_search = None
+            msg.append("Keyword research.'")
+
+        if page_exporter is not None and offer.timestamp_is_too_old(offer.URLInspectionTimeStamp.DOWNLOAD, threshold):
+            page_exporter = None
+            msg.append("Offer downloading.'")
+
+        if offer.timestamp_is_too_old(offer.URLInspectionTimeStamp.TEXT, threshold):
+            search_offer_text = False
+            msg.append("Offer's text parsing.")
+
+        if offer.timestamp_is_too_old(offer.URLInspectionTimeStamp.HTML, threshold):
+            search_offer_html = False
+            msg.append("Offer's HTML parsing'.")
+
+        if msg:
+            cls.logger.info(
+                "Ignoring step(s) realised less than %s days ago during offer processing of '%s' (%s): "
+                "\n-\t%s"
+                "\nThreshold date is '%s'",
+                re_inspect_after,   offer, offer_id + 1,
+                "\n-\t".join(msg),
+                threshold,
+            )
+
+        return keywords_to_search, page_exporter, search_offer_text, search_offer_html
 
     # TOOLS
     @classmethod
@@ -192,9 +350,19 @@ class JobScrapperSkeleton(ScrapperRequestCore):
 
         return field
 
+    def timestamp_is_too_old(
+            self,
+            ts_type: ScrapperRequestCore.URLInspectionTimeStamp,
+            threshold: datetime
+    ) -> bool:
+        if not self.time_stamps_exist(ts_type):
+            return False
+
+        ts = self.retrieve_time_stamps(self.URLInspectionTimeStamp.DOWNLOAD)
+        dt = datetime.fromtimestamp(time.mktime(ts))
+        return threshold <= dt
 
     # QUESTIONS
-
     @classmethod
     def get_offer_listing_url(cls) -> str:
         """Returns a url that lead to an online listing of offers."""
