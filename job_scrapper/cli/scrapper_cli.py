7df86d78c03c6f10b66d14597412e70e36902143
F-Marchal <marchal.florent.pro@gmail.com>
2026-01-06 12:29:51 +0100

    feat(cli---scrap): Adapt scrap command to the new structure
    #51

--- job_scrapper/cli/scrapper_cli.py
+++ job_scrapper/cli/scrapper_cli.py
@@ -1,207 +1,143 @@
 import click
 import cloup
+from cli.ask_contact import CONTACT_OPTION, ask_contact
+from job_scrapper import SCRAPER_REGISTRY, JobScrapperSkeleton
 
-from job_scrapper import (
-    CHUMtpScrapper,
-    CiradScrapper,
-    CNRScrapper,
-    FranceGenomiqueScrapper,
-    IfremerScrapper,
-    INRAEScrapper,
-    InsermScrapper,
-    IRDScrapper,
-    JobScrapperSkeleton,
-    SanofiScrapper,
-    SFBIScrapper,
+@cloup.command()
+@cloup.pass_context
+@cloup.argument(
+    "scrappers",
+    required=True,
+    type=click.Choice(
+        list(sorted(SCRAPER_REGISTRY.keys()))
+    ),
+    help="Select which website you want to scrap.",
+    nargs=-1
 )
 
-@cloup.group()
+@CONTACT_OPTION
 @cloup.option(
     "-r",
-    "--ref-places",
-    type=click.File("r"),
-    default=None,
-    help="A JSON file containing a list of places. The program will compute the distances (km) that separate"
-    'a job offer from those places. E.g. ["Paris, France", "London, UK", "Sheffield, UK"]'
-    "By default Workdir/researched-localisation",
+    "--reinspect_offer_after",
+    type=int,
+    default=14,
+    help="Number of days to wait before reopening the web page containing an offer. "
+        "This prevents repeatedly accessing the same offer too frequently."
 )
 @cloup.option(
-    "-k",
-    "--keywords",
-    type=click.File("r"),
-    default=None,
-    help="A JSON file containing a number of keywords. The program will seek and count those keywords inside each job offer."
-    'The JSON file should look like : {"keywords": ["keyword", "alias1", "alias2"...]} '
-    'E.g {"Artificial intelligence": ["Artificial intelligence", "IA", "LLM", "Neural Networks"]} In this example,'
-    ' each occurrences of "Artificial intelligence", "IA", "LLM", "Neural Networks" will increment the counter'
-    'By default "Workdir/researched-keywords.json"',
-)
-@cloup.option(
-    "-c",
-    "--coordinates",
-    type=click.File("r"),
-    default=None,
-    help="A JSON file containing a number of places associated with there coordinate (latitude, longitude). "
-    "This will speed up the the program when it compute the distances (km) that separate a job offer from a place. "
-    "At the end of the program, this file will be updated with newly encountered localisation. Use "
-    "`--no-coordinates-update` to disable this behaviour\n"
-    'The JSON file should look like : {"places": [latitude, longitude]}. '
-    'E.g  {"Paris, France": [48.8534951, 2.3483915]}.\n'
-    'By default "Workdir/known-localisation.json" is used',
-)
-@cloup.option(
-    "-i",
-    "--ignore-urls",
-    type=click.File("r"),
-    default=None,
-    help="A JSON file containing a number of urls. When the job offer's url is contained inside it, the following "
-    "processing will not occurred : 1. keywords researched 2. distances computation 3. html download."
-    "At the end of the program, this file will be updated with newly encountered url. Use "
-    "`--no-urls-update` to disable this behaviour."
-    'The JSON file should look like :  ["urls"].'
-    'By default "Workdir/.known-urls.json" is used.',
-)
-@cloup.option(
-    "--no-coordinates-update",
+    "-t",
+    "--tsv",
     is_flag=True,
-    help="Turn this flag on to disable coordinates's json updates",
+    help="Also export offers inside TSV files. Those files will be located inside "
+         "`[Workdir]/[Scrapper Name]/TSVs/`",
 )
+
 @cloup.option(
-    "--no-urls-update",
-    is_flag=True,
-    help="Turn this flag on to disable ignore-urls's json updates ",
-)
-@cloup.option(
-    "-s",
-    "--save-job-page",
-    is_flag=True,
-    help="Job's web pages will be download and stored inside the workdir.",
-)
-@cloup.option(
-    "-d",
-    "--display",
-    is_flag=True,
-    help="Output the result inside the terminal. If --result-file (-f) is not used and "
-    "--no-sql-export (-q) is used, this flag"
-    "is turned on by default.",
+    "-b",
+    "--batch-export",
+    type=int,
+    default=20,
+    help="Group offer exportation to TSV / the database to limit how many time tsv file / database are opened."
+         "Use `-b 0` to disable batch exportation.",
 )
 @cloup.option(
     "-f",
-    "--result-file",
-    type=click.Path(file_okay=True, dir_okay=False, resolve_path=True),
-    default=None,
-    help="Write the result of the scrapping inside a file. /!\\ If this file exist, this file will be overwritten.",
+    "--retry-webpage-fetch",
+    type=int,
+    default=2,
+    help="How many time the scrapper can retry to fetch an url.",
 )
 @cloup.option(
     "-q",
-    "--no-sql-export",
-    is_flag=True,
-    help="The result will not be exported inside a database in the workdir.",
+    "--sleep-before-retry",
+    type=float,
+    default=2,
+    help="How long (in seconds) the scrapper should wait before retrying to fetch an url. (Minimum=0.25 seconds)",
 )
-@cloup.pass_context
-# pylint: disable=R0913
-# pylint: disable=R0917
-# having many arguments is expected for this command
+@cloup.option(
+    "-d",
+    "--download",
+    type=click.Choice([s.value for s in JobScrapperSkeleton.SaveTypes]),
+    default=None,
+    help="Will trigger offer download. Offers will be downloads in [Workdir]/[Scrapper Name]/. "
+         "Text : Only store text extracted from the offer ; HTML : Store full HTML page or the file that contains the "
+         "offer (.pdf...) ; MHTML : Full page storage with images and more. You might need Google Chrome to "
+         "open this format of file.",
+)
+@cloup.option(
+    "--compress-download",
+    is_flag=True,
+    help="Should downloaded file (-d) be compressed into a zip archive ?",
+)
+
+
+
 def scrap(
-    ctx,
-    ref_places,
-    keywords,
-    coordinates,
-    ignore_urls,
-    no_coordinates_update,
-    no_urls_update,
-    save_job_page,
-    display,
-    result_file,
-    no_sql_export,
+        ctx,
+        scrappers: list[str],
+        tsv: bool,
+        batch_export: int,
+        sleep_before_retry: float,
+        reinspect_offer_after: int,
+        retry_webpage_fetch: int,
+        compress_download: bool,
+        contact: str = "",
+        download: JobScrapperSkeleton.SaveTypes | None = None,
 ):
-    """Scraps a specific website (see command section) to extract job offers. By default,
-    the results are exported inside a sqlite database.
-    General information can be retrieved from all website such as job name, job localisation, job field, and more.
-    More specific information like job distance to a certain place (--ref-places) or the number of occurrences of
-    certain keyword can also be obtains (--keywords)."""
-    JobScrapperSkeleton.logger.debug("Scrap : %s", locals())
-    JobScrapperSkeleton.set_workdir(ctx.obj["workdir"])
-    ctx.obj["OptionsScrapperMain"] = {
-        "localisations_to_search_json": ref_places,
-        "keywords_to_search_json": keywords,
-        "known_localisations_json": coordinates,
-        "known_urls_json": ignore_urls,
-        "dump_localisations": not no_coordinates_update,
-        "dumb_urls": not no_urls_update,
-        "save_job_page": save_job_page,
-        "display": display or (no_sql_export and not result_file),
-        "flat_export": result_file,
-    }
+    """
+    Extract job offer from one (or more) website. Extractions are
+    run sequentially.
+    1) Extract a list of keyword to search inside offer from
+    the database in workdir (`-w`) ;
+    2) Connect to selected websites and parse listing to extract
+    offers ;
+    3) For each offer, this program will fetch offer's url and
+    download webpage content (file / html) inside a temporary
+    folder. Then, keywords are search inside the content of
+    the offer. If `-d`, the file is saved in the workdir (`-w`);
+    4) Geopy is used to find the geographic coordinates
+    of the location associated with a job posting. This step
+    might be step if  the location associated with a job posting
+    is stored in the database ;
+    5) Job offer is exported to the database ;
+    """
+    # Ensure we have contact information
+    if not contact:
+        contact = ask_contact(ctx.obj["workdir"])
 
-@scrap.command()
-@cloup.pass_context
-def chu_mpt(ctx):
-    """Scraps Montpellier CHU's job offer."""
-    CHUMtpScrapper.main(**ctx.obj["OptionsScrapperMain"])
+    retry_webpage_fetch = max(retry_webpage_fetch, 0)
+    reinspect_offer_after = max(reinspect_offer_after, 0)
+    sleep_before_retry = max(sleep_before_retry, 0.25)
 
+    if download:
+        ebp = JobScrapperSkeleton.get_page_exporter(
+            save_type=download,
+            compress=compress_download,
+        )
+    else:
+        ebp = None
 
-@scrap.command()
-@cloup.pass_context
-def cirad(ctx):
-    """Scraps Cirad's website. ('Centre de coopération internationale en recherche agronomique pour le développement')"""
-    CiradScrapper.main(**ctx.obj["OptionsScrapperMain"])
+    for scrapper_name in scrappers:
+        scrapper = SCRAPER_REGISTRY[scrapper_name]
 
+        scrapper.run(
+            contact=f"{contact}-(Running job-scrapper {scrapper_name})",
+            page_exporter=ebp,
 
-@scrap.command()
-@cloup.pass_context
-def cnrs(ctx):
-    """Scrap CNRS job offers 'Centre national de la recherche scientifique'"""
-    CNRScrapper.main(**ctx.obj["OptionsScrapperMain"])
+            sql_export=True,
+            tsv_export=tsv,
 
+            search_offer_text=True,
+            search_offer_html=True,
+            keywords_to_search=None,
 
-@scrap.command()
-@cloup.pass_context
-def france_genomique(ctx):
-    """Scraps jobs from France Genomics's website. Not tested with many offers !!"""
-    FranceGenomiqueScrapper.main(**ctx.obj["OptionsScrapperMain"])
-
-
-@scrap.command()
-@cloup.pass_context
-def ifremer(ctx):
-    """Scrapss jobs from ifremer. 'French research institute for the exploitation of maritime environment'"""
-    IfremerScrapper.main(**ctx.obj["OptionsScrapperMain"])
+            retry_offer_fetch=retry_webpage_fetch,
+            failed_sleep=sleep_before_retry,
 
 
-@scrap.command()
-@cloup.pass_context
-def inrae(ctx):
-    """Scrapss jobs from INRAE's website. 'Institut national de recherche pour l’agriculture,
-    l’alimentation et l’environnement'"""
-    INRAEScrapper.main(**ctx.obj["OptionsScrapperMain"])
+            database_name=None,
+            workdir=ctx.obj["workdir"],
 
-
-@scrap.command()
-@cloup.pass_context
-def inserm(ctx):
-    """Scraps jobs from INSERM's ('Institut national de
-    la santé et de la recherche médicale') website. 'Mobilité Chercheurs', 'CDD Chercheurs'
-    and 'Mobilité Ingénieurs et Techniciens' are ignored."""
-    InsermScrapper.main(**ctx.obj["OptionsScrapperMain"])
-
-
-@scrap.command()
-@cloup.pass_context
-def ird(ctx):
-    """Scraps jobs from IRD's website. ('Institut de recherche pour le développement')"""
-    IRDScrapper.main(**ctx.obj["OptionsScrapperMain"])
-
-
-@scrap.command()
-@cloup.pass_context
-def sanofi(ctx):
-    """Scraps Sanofi's website (Job offers are limited to 'France')."""
-    SanofiScrapper.main(**ctx.obj["OptionsScrapperMain"])
-
-
-@scrap.command()
-@cloup.pass_context
-def sfbi(ctx):
-    """Scraps SFBI's website. ('Société Française de Bioinformatique')"""
-    SFBIScrapper.main(**ctx.obj["OptionsScrapperMain"])
+            batch_export=batch_export,
+            re_inspect_after=reinspect_offer_after,
+        )
