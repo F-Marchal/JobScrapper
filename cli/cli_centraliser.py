88ddb692ad172864aa767f7b6b222309e2a6d5b8
F-Marchal <marchal.florent.pro@gmail.com>
2026-01-06 12:32:25 +0100

    feat(cli_centraliser): Update centraliser to acomodate new cli
    #51

--- cli/cli_centraliser.py
+++ cli/cli_centraliser.py
@@ -1,6 +1,12 @@
 from job_scrapper.cli.scrapper_cli import scrap
-from job_scrapper.cli.database_cli import database
+# from sql.cli.enumerate_cli import enumerate
+from cli.ask_contact import configure_contact
 from cli.cli_base import cli
-
+from sql.cli.keyword_cli import manage_keywords
+from sql.cli.request_job_cli import request, request_format_help
+cli.add_command(configure_contact)
+cli.add_command(manage_keywords)
 cli.add_command(scrap)
-cli.add_command(database)
+cli.add_command(request_format_help)
+cli.add_command(request)
+# cli.add_command(enumerate)
