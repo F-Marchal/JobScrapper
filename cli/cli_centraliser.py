eaca1fc567263cc85299f534f1532f921fcb1d61
F-Marchal <marchal.florent.pro@gmail.com>
2026-01-07 11:02:32 +0100

    feat(cli---geolocalisation): Every cli related to places manipulation in database
    #53

--- cli/cli_centraliser.py
+++ cli/cli_centraliser.py
@@ -4,9 +4,11 @@ from cli.ask_contact import configure_contact
 from cli.cli_base import cli
 from sql.cli.keyword_cli import manage_keywords
 from sql.cli.request_job_cli import request, request_format_help
+from sql.cli.localisation_cli import geolocalisation
 cli.add_command(configure_contact)
 cli.add_command(manage_keywords)
 cli.add_command(scrap)
 cli.add_command(request_format_help)
 cli.add_command(request)
+cli.add_command(geolocalisation)
 # cli.add_command(enumerate)
